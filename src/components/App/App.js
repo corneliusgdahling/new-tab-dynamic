9979afbca6c1b096e728b731e8af76120b291d79
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2017-06-04 20:53:40 +0100

    Cleanup

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{ display: 'flex' }}>
+        <h1 style={{ margin: 'auto' }}>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App
