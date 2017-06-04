3277e24f6fef5206f8b8141fb5d1ac2938fe6a3e
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2017-06-04 23:12:18 +0100

    Dependency for material ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 import App from './components/App/App'
 import './index.css'
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
+  injectTapEventPlugin(),
 )
