f339bf9088c04924a9dfbb7e329c5649bb526956
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2017-06-05 22:50:21 +0100

    Added card that can be modified with localStorage

--- src/components/Shortcuts/ShortcutCard/ShortcutCard.js
+++ src/components/Shortcuts/ShortcutCard/ShortcutCard.js
@@ -1,46 +1,92 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card'
-import FlatButton from 'material-ui/FlatButton';
+import { Card, CardActions, CardMedia, CardTitle } from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
+import Dialog from 'material-ui/Dialog'
+import TextField from 'material-ui/TextField'
 
 class ShortcutCard extends Component {
   constructor() {
     super()
     this.state = {
-      cardUrl: '',
-      imageUrl: '',
+      cardName: localStorage.getItem('cardName') ? localStorage.getItem('cardName') : 'Undefined',
+      cardUrl: localStorage.getItem('cardUrl') ? localStorage.getItem('cardUrl') : 'chrome://newtab',
+      imageUrl: localStorage.getItem('imageUrl') ? localStorage.getItem('imageUrl') : 'https://www.iconfinder.com/data/icons/huge-black-icons/512/Help.png',
       editMode: false,
-
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit() {
+    localStorage.setItem('cardName', this.state.cardName)
+    localStorage.setItem('cardUrl', this.state.cardUrl)
+    localStorage.setItem('imageUrl', this.state.imageUrl)
+    this.setState({ editMode: !this.state.editMode })
   }
 
   render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary
+        onTouchTap={() => this.setState({ editMode: !this.state.editMode })}
+      />,
+      <FlatButton
+        label="Submit"
+        primary
+        onTouchTap={this.handleSubmit}
+      />,
+    ]
     return (
-      <MuiThemeProvider>
-        <Card>
-          <CardHeader
-            title="URL Avatar"
-            subtitle="Subtitle"
-            avatar="images/jsa-128.jpg"
-          />
-          <CardMedia
-            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
-          >
-            <img src={this.state.imageUrl} alt="" />
-          </CardMedia>
-          <CardTitle title="Card title" subtitle="Card subtitle" />
-          <CardText>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <CardActions>
-            <FlatButton label="Action1" />
-            <FlatButton label="Action2" />
-          </CardActions>
-        </Card>
-      </MuiThemeProvider>
+      <div>
+        <MuiThemeProvider>
+          <Card style={{ borderRadius: '15px' }}>
+            <a href={this.state.cardUrl}>
+              <CardMedia
+                overlay={
+                  <CardTitle title={this.state.cardName} subtitle={this.state.cardUrl} />
+                }>
+                <img src={this.state.imageUrl} alt="" style={{ borderTopRightRadius: '15px', borderTopLeftRadius: '15px' }} />
+              </CardMedia>
+            </a>
+            <CardActions>
+              {!this.state.editMode &&
+                <FlatButton
+                  label="Edit"
+                  onClick={() => this.setState({ editMode: !this.state.editMode })} />
+              }
+            </CardActions>
+          </Card>
+        </MuiThemeProvider>
+        <MuiThemeProvider>
+          <Dialog
+            title="Add or modify shortcut"
+            modal
+            actions={actions}
+            open={this.state.editMode}>
+            <TextField
+              value={this.state.cardName === 'Undefined' ? '' : this.state.cardName}
+              floatingLabelText="Shortcut name"
+              floatingLabelFixed
+              onChange={(e, v) => this.setState({ cardName: v })}
+              />
+            <TextField
+              style={{ color: '#FFF' }}
+              value={this.state.cardUrl === 'chrome://newtab' ? '' : this.state.cardUrl}
+              floatingLabelText="Url (link/webpage) for shortcut"
+              floatingLabelFixed
+              onChange={(e, v) => this.setState({ cardUrl: v })}
+            />
+            <TextField
+              value={this.state.imageUrl === 'https://www.iconfinder.com/data/icons/huge-black-icons/512/Help.png' ? '' : this.state.imageUrl}
+              floatingLabelText="Url (link/webpage) for background image"
+              floatingLabelFixed
+              fullWidth
+              onChange={(e, v) => this.setState({ imageUrl: v })}
+            />
+          </Dialog>
+        </MuiThemeProvider>
+      </div>
     )
   }
 }
