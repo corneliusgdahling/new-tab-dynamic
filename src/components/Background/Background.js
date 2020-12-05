6f34fed7aa6e881a92139755c7123315ffcd9e16
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2020-12-05 23:41:18 +0100

    error handling

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -7,23 +7,23 @@ const URL = 'https://pixabay.com/api/?key=5546451-397d91c91b993dc32692557b3&q='
 const URL_CONFIG = '&image_type=photo&pretty=true'
 
 const getBackgroundUrl = async searchTerm => {
-  console.log({searchTerm})
   const response = await fetch(URL + searchTerm + URL_CONFIG, {
     method: 'GET',
     mode: 'cors',
   })
   if (!response.ok) throw Error()
   const results = await response.json()
-  const url = results.hits[Math.round(Math.random() * results.hits.length - 1)].webformatURL
-  console.log('url', url)
+  const randNum = Math.random() * results.hits.length - 1
+  if (!results.hits[Math.round(randNum > 0 ? randNum : 0)]) {
+    return
+  }
+  const url = results.hits[Math.round(randNum > 0 ? randNum : 0)].webformatURL
   return url
 }
 
 export const Background = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [backgroundUrl, setBackgroundUrl] = useState(localStorage.getItem('backgroundUrl') || '')
-
-  console.log('backgroundURL', backgroundUrl)
 
   return (
     <div className="gridContainer" style={{ backgroundImage: `url(${backgroundUrl})` }}>
