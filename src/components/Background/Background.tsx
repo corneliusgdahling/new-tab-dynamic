2b0480537712a183922568d3f03592c846e675dc
Cornelius Grieg Dahling <corneliusgdahling@gmail.com>
2020-12-30 17:17:39 +0100

    Can navigate backgrounds

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import Backdrop from '@material-ui/core/Backdrop'
 import Fab from '@material-ui/core/Fab'
 import ImageSearchIcon from '@material-ui/icons/ImageSearch'
+import ArrowRightIcon from '@material-ui/icons/ArrowRight'
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft'
 import CloseIcon from '@material-ui/icons/Close'
 import './Background.css'
 
@@ -9,18 +11,23 @@ const URL = 'https://pixabay.com/api/?key=5546451-397d91c91b993dc32692557b3&q='
 
 const URL_CONFIG = '&image_type=photo'
 
-const getBackgroundUrl = async (searchTerm: string) => {
-  const response = await fetch(URL + searchTerm + URL_CONFIG, {
+const getImageUrl = (searchTerm: string, page = 1) => {
+  return `${URL}${searchTerm}${URL_CONFIG}&page=${page}`
+}
+
+const getBackgroundUrl = async (searchTerm: string, resultNumber = 0) => {
+  const resultNumberToUse = resultNumber - 1 > 0 ? resultNumber : 0
+  const page = Math.floor(resultNumberToUse / 20) + 1
+  const imageUrl = getImageUrl(searchTerm, page)
+  const response = await fetch(imageUrl, {
     method: 'GET',
     mode: 'cors',
   })
   if (!response.ok) throw Error()
   const results = await response.json()
-  const randNum = Math.random() * results.hits.length - 1
-  if (!results.hits[Math.round(randNum > 0 ? randNum : 0)]) {
-    return
-  }
-  const url = results.hits[Math.round(randNum > 0 ? randNum : 0)].largeImageURL
+  if (resultNumberToUse >= results.totalHits) return
+  const resultOnPage = resultNumberToUse % 20
+  const url = results.hits[resultOnPage].largeImageURL
   return url
 }
 
@@ -30,23 +37,35 @@ interface BackgroundInterface {
 
 export const Background: React.FC<BackgroundInterface> = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('')
-  const [backgroundUrl, setBackgroundUrl] = useState(
-    localStorage.getItem('backgroundUrl') || ''
-  )
+  const [resultNumber, setResultNumber] = useState(1)
+  const [backgroundUrl, setBackgroundUrl] = useState('')
 
   const [editBackground, setEditBackground] = useState(false)
 
   const inputRef = useRef<HTMLInputElement>(null)
 
-  const setBackground = async (searchTerm: string) => {
-    const url = await getBackgroundUrl(searchTerm)
-    setBackgroundUrl(url)
-    localStorage.setItem('backgroundUrl', url)
+  const setBackground = async (searchTerm: string, resultNumber = 0) => {
+    const url = await getBackgroundUrl(searchTerm, resultNumber)
+    if (url) {
+      setBackgroundUrl(url)
+      localStorage.setItem('backgroundUrl', url)
+    } else {
+      setResultNumber(1)
+      setBackground(searchTerm, 1)
+    }
   }
+
+  useEffect(() => {
+    setBackgroundUrl(localStorage.getItem('backgroundUrl') || '')
+  })
 
   useEffect(() => {
     if (editBackground && inputRef?.current) inputRef.current.focus()
   }, [editBackground, inputRef])
+
+  useEffect(() => {
+    if (backgroundUrl) setBackground(searchTerm, resultNumber)
+  }, [resultNumber])
 
   return (
     <>
@@ -60,19 +79,39 @@ export const Background: React.FC<BackgroundInterface> = ({ children }) => {
         </Fab>
       </div>
       <Backdrop open={editBackground} transitionDuration={500}>
-        <input
-          ref={inputRef}
-          className="input"
-          placeholder="Søkeord"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setSearchTerm(e.target.value)
-          }
-          onKeyDown={async (e) => {
-            if (e.key === 'Enter') setBackground(searchTerm)
-            else if (e.key === 'Escape') setEditBackground(false)
-          }}
-          value={searchTerm}
-        />
+        <div className="backgroundSelector">
+          <input
+            ref={inputRef}
+            className="input"
+            placeholder="Søkeord"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setSearchTerm(e.target.value)
+            }}
+            onKeyDown={async (e) => {
+              if (e.key === 'Enter')
+                setBackground(searchTerm) && setResultNumber(1)
+              else if (e.key === 'Escape') setEditBackground(false)
+            }}
+            value={searchTerm}
+          />
+          <div className="backgroundIncrementContainer">
+            <button
+              onClick={() => setResultNumber(resultNumber - 1)}
+              className="backgroundIncrement backgroundIncrease"
+            >
+              <ArrowLeftIcon />
+            </button>
+            <span className="backgroundIncrement pageNumber">
+              <h2>{resultNumber}</h2>
+            </span>
+            <button
+              onClick={() => setResultNumber(resultNumber + 1)}
+              className="backgroundIncrement backgroundDecrease"
+            >
+              <ArrowRightIcon />
+            </button>
+          </div>
+        </div>
       </Backdrop>
       <>{!editBackground && children}</>
     </>
